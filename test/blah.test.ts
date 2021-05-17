fa6a72a0ac18a1ddfb03026ba1f916a73b6d251a
wangye <wy>
2021-05-17 19:10:57 +0800

    fix: 修改测试用例

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,7 +1,26 @@
-import { sum } from '../src';
+import { accAdd, accDiv, accSub, accMul } from '../src';
 
 describe('blah', () => {
-  it('works', () => {
-    expect(sum(1, 1)).toEqual(2);
-  });
+    it('works', () => {
+        expect(accAdd(1, 1)).toEqual(2);
+    });
+});
+
+describe('blah', () => {
+    it('works', () => {
+        expect(accDiv(10, 2)).toEqual(5);
+    });
+});
+
+
+describe('blah', () => {
+    it('works', () => {
+        expect(accSub(10, 2)).toEqual(8);
+    });
+});
+
+describe('blah', () => {
+    it('works', () => {
+        expect(accMul(10, 2)).toEqual(20);
+    });
 });
